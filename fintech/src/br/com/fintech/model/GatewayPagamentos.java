e52afa859706371b258a45edd7731262431e5c5c
Adriel Wanderlind <adrielwanderlind80@gmail.com>
2026-09-15 17:42:41 -0300

    Add files via upload

---
+++ fintech/src/br/com/fintech/model/GatewayPagamentos.java
@@ -0,0 +1,16 @@
+package br.com.fintech.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GatewayPagamentos {
+    private Map<String, CarteiraDigital> carteiras = new HashMap<>();
+    
+    public void cadastrarCarteira(CarteiraDigital carteira) {
+    this.carteiras.put(carteira.getCodigoCarteira(), carteira);
+    }
+    
+    public CarteiraDigital buscarCarteira(String codigoCarteira) {
+        return this.carteiras.get(codigoCarteira);
+    }
+}
