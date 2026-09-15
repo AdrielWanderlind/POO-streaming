7e1c9e34c3e491137c720021880c3343e41ab83a
Adriel Wanderlind <adrielwanderlind80@gmail.com>
2026-09-15 17:42:26 -0300

    Delete fintech/fintech/src/br/com/fintech directory

--- fintech/fintech/src/br/com/fintech/exception/LimiteTransacoesExcedidoException.java
+++
@@ -1,9 +0,0 @@
-package br.com.fintech.exception;
-
-public class LimiteTransacoesExcedidoException extends Exception{
-
-    public LimiteTransacoesExcedidoException(String mensagem) {
-        super(mensagem);
-    }
-
-}
