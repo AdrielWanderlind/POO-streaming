3cface576d620dabd1e7d9283a38996ece8b43d9
Adriel Wanderlind <adrielwanderlind80@gmail.com>
2026-09-15 19:01:29 -0300

    Add files via upload

---
+++ missao-08-streams/src/br/com/technexus/model/Loja.java
@@ -0,0 +1,36 @@
+package br.com.technexus.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loja {
+    private List<Produto> catalogo = new ArrayList<>();
+
+    public void cadastrar(Produto p){
+        this.catalogo.add(p);
+    }
+
+    public List<Produto> buscarPorCategoria(String catDesejada) {
+        return this.catalogo.stream()
+                .filter(p -> p.getCategoria().equalsIgnoreCase(catDesejada))
+                .toList();
+    }
+
+    public double calcularPatrimonioTotal() {
+        return this.catalogo.stream()
+                .mapToDouble(p ->p.getPreco())
+                .sum();
+    }
+
+    public double calcularTotalPorCategoria(String catDesejada) {
+        return this.catalogo.stream()
+        .filter(p->p.getCategoria().equalsIgnoreCase(catDesejada))
+        .mapToDouble(p->p.getPreco())
+        .sum();
+    }
+
+    public void cadastrar(String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrar'");
+    }
+}
